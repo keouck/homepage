c95626d39377ea4f6607fda97e797f28c02c12cc
balzaen <imerminu@tutanota.com>
2022-06-12 22:35:53 +0530

    Create navbar without branding

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template('index.html')
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+@app.route('/about')
+def about():
+	return render_template('about.html')
+@app.route('/blogs')
+def blogs():
+	return render_template('blogs.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
