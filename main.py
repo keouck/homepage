30ad243682183cde3e48705dbcec697b404f8fe7
keouck <imerminu@tutanota.com>
2023-10-28 13:05:35 +0530

    Redesigned yet again

--- main.py
+++ main.py
@@ -10,17 +10,13 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/blogs')
-def blogs():
-    return render_template('blogs.html')
-
 @app.route('/shenanigans')
 def shenanigans():
     return render_template('shenanigans.html')
 
-@app.route('/devnull')
-def devnull():
-    return render_template('devnull.html')
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True)
